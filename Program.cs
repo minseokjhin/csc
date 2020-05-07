a6f6be50c7176b77e3bc59329b5442e6ce6c250c
JHIN Greg -DARLINGTON <greg.jhin@opg.com>
2020-05-07 14:54:45 -0400

    commit on master

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace csc
             Console.WriteLine("Hello World!");
             Console.WriteLine("I've made a change")
             Console.WriteLine("I am going create a branch my-new-branch")
+            console.WriteLine("VS code commit")
         }
     }
 }
